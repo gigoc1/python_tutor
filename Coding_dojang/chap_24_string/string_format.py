0d8c941485c2f676431ff19012913b29a8db7b5e
gigoc1 <gigoc1@naver.com>
2022-08-02 01:01:24 +0900

    coding dojang chap_25 adding'1

--- Coding_dojang/chap_24_string/string_format.py
+++ Coding_dojang/chap_24_string/string_format.py
@@ -6,6 +6,6 @@ print('Today is %d %s.' % (3, 'April'))
 
 print('\n- format 메서드')
 print('Hello, {0} {2} {1}'.format('Python', 'Script', 3.6))
-print('{0:0<10}'.format('python'))
+print('{0:0<10}'.format('python')) #'{인덱스:[[채우기]정렬][길이][.자릿수][자료형]}'
 print('{0:->10}'.format('python'))
 print('{0:08.2f}'.format(150.37))
