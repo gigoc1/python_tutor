18ea0f2c13ece617f3bf95017c9e01b9a1b07b96
gigoc1 <gigoc1@naver.com>
2022-08-04 02:25:49 +0900

    2nd commit coding dojang

--- Coding_dojang/chap_26_set/expression_set.py
+++ Coding_dojang/chap_26_set/expression_set.py
@@ -1,3 +1,10 @@
 # for 반복문을 통한 세트 생성
 # {식 for 변수 in 반복가능한객체}
 # set(식 for 변수 in 반복가능한객체)
+a = {i for i in 'apple'}
+print(a)
+
+# 세트 표현식에 if 사용
+# {식 for 변수 in 세트 if 조건식}
+# set(식 for 변수 in 세트 if 조건식)
+print({i for i in 'pineapple' if i not in 'apl'})
