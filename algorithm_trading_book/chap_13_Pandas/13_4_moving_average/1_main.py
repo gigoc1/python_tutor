1961307355e68c9355b6b8435718ba620f426b5f
gigoc1 <gigoc1@naver.com>
2022-08-23 14:38:48 +0900

    taeyoung schedule adding

--- algorithm_trading_book/chap_13_Pandas/13_4_moving_average/1_main.py
+++ algorithm_trading_book/chap_13_Pandas/13_4_moving_average/1_main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import pandas_datareader.data as web
-gs = web.DataReader("TSLA", "yahoo", "2022-01-01")
+from pandas import to_numeric
+gs = web.DataReader("361610 ", "naver", "2022-01-01")
+gs = gs.apply(to_numeric) #naver는 데이터가 string이여서 numeric로 변경 필요. yahoo는 필요없음
 # print(gs.tail())
-ma5=gs['Adj Close'].rolling(window=5).mean()
-ma20=gs['Adj Close'].rolling(window=20).mean()
-ma60=gs['Adj Close'].rolling(window=60).mean()
-ma120=gs['Adj Close'].rolling(window=120).mean()
+ma5=gs['Close'].rolling(window=5).mean()  #yahoo는 'Adj Close', naver는 'Close'
+ma20=gs['Close'].rolling(window=20).mean()
+ma60=gs['Close'].rolling(window=60).mean()
+ma120=gs['Close'].rolling(window=120).mean()
 # print(ma5.tail(10))
 # print(gs['Volume']!=0), 공휴일(거래량=0) 데이터가 원본에 없어서 코드는 무의미 함
 gs.insert(len(gs.columns), "MA5", ma5)
@@ -15,7 +17,7 @@ gs.insert(len(gs.columns), "MA120", ma120)
 print(gs.tail(20))
 
 import matplotlib.pyplot as plt
-plt.plot(gs.index, gs['Adj Close'], label="Adj Close")
+plt.plot(gs.index, gs['Close'], label="Close")
 plt.plot(gs.index, gs['MA5'], label='MA5')
 plt.plot(gs.index, gs['MA20'], label='MA20')
 plt.plot(gs.index, gs['MA60'], label='MA60')
